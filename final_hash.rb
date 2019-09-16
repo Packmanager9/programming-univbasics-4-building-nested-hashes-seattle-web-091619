64a846cf3310b764cba1079dbbc56862423e36bf
Cole Ditzler <colejditzler@gmail.com>
2019-09-16 09:12:26 +0000

    Automatically backed up by Learn

--- final_hash.rb
+++ final_hash.rb
@@ -2,16 +2,6 @@
 # you're suppose to "build up" to this.
 def base_hash
 monopoly =  {
-  :railroads=>
-  {
-    :pieces=>4,
-    :rent_in_dollars=>
-    {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
-    },
-  }
+  :railroads=>{}
 }
 end
